585b0e6f6147efc21af9a7c454b692080cd35b6b
Hiroshi Hashiguchi <dev@5tec.jp>
2011-05-13 23:10:07 +0900

    Initial Commit

---
+++ LKKeychain/LKKeychainAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  LKKeychainAppDelegate.h
+//  LKKeychain
+//
+//  Created by Hiroshi Hashiguchi on 11/05/13.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LKKeychainAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ LKKeychainTests/LKKeychainTests.h
@@ -0,0 +1,17 @@
+//
+//  LKKeychainTests.h
+//  LKKeychainTests
+//
+//  Created by Hiroshi Hashiguchi on 11/05/13.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface LKKeychainTests : SenTestCase {
+@private
+    
+}
+
+@end
